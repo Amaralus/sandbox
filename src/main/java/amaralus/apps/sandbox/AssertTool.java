b6f4b987071e19b4141d0d97519dab36e2fa974a
Evgenii Dolgov <river100@mail.ru>
2020-07-15 16:18:20 +0300

    upgrade assert tool

--- src/main/java/amaralus/apps/sandbox/AssertTool.java
+++ src/main/java/amaralus/apps/sandbox/AssertTool.java
@@ -10,43 +10,54 @@ import java.util.Objects;
 @Slf4j
 public class AssertTool {
 
-    private boolean skipMissingFields;
+    private boolean skipMissingFields = false;
+    private StringBuilder failedAssertBuilder;
 
-    public AssertTool() {
-        this(false);
-    }
+    /**
+     * Проверяет поля объектов на equals, в случает если они не совпадают
+     * записывает ошибку в журнал, который можно извлечь позже. Так же
+     * имеет возможность проверять начличие полей в проверяемом объекте
+     * с записью в журнал если поле не будет найдено.
+     *
+     * @param expected Ожидаемый эталонный объект.
+     * @param actual   Фактический объект котоырй нужно проверить.
+     * @return Возвращает true когда все полля совпадают и есть в проверяемом объекте (если включено).
+     */
+    public boolean assertEquals(Object expected, Object actual) {
+        Objects.requireNonNull(expected, "expected object must be not null!");
+        Objects.requireNonNull(actual, "actual object must be not null!");
 
-    public AssertTool(boolean skipMissingFields) {
-        this.skipMissingFields = skipMissingFields;
-    }
-
-    public boolean assertThat(Object expected, Object actual) {
         try {
+            failedAssertBuilder = new StringBuilder();
             boolean result = true;
             Map<String, Object> actualFieldsValues = extractFieldsValues(actual);
 
             for (Map.Entry<String, Object> entry : extractFieldsValues(expected).entrySet()) {
                 if (!actualFieldsValues.containsKey(entry.getKey())) {
-                    if (skipMissingFields)
-                        log.info("Отсутствующеее поле {} пропущено", entry.getKey());
-                    else {
+                    if (!skipMissingFields) {
                         result = false;
-                        log.info("Ожидаемое поле {} отсутсвует", entry.getKey());
+                        addFailedAssert(String.format("Ожидаемое поле %s отсутсвует", entry.getKey()));
                     }
-
                 } else if (!Objects.equals(entry.getValue(), actualFieldsValues.get(entry.getKey()))) {
-                    log.info("Для поля {} ожидалось значение {}, но было {}",
-                            entry.getKey(), entry.getValue(), actualFieldsValues.get(entry.getKey()));
                     result = false;
+                    addFailedAssert(String.format("Для поля %s ожидалось значение %s, но было %S",
+                            entry.getKey(), entry.getValue(), actualFieldsValues.get(entry.getKey())));
                 }
             }
             return result;
         } catch (IllegalAccessException e) {
-            log.error("assert error", e);
-            return false;
+            log.error("Ошибка доступа во время сравнения объектов", e);
+            throw new IllegalStateException(e);
         }
     }
 
+    /**
+     * Извлекает из объекта поля и их значения в виде мапы.
+     *
+     * @param object Объект из которого нужно вытащить значения полей.
+     * @return Возвразает мапу с полями и их значениями.
+     * @throws IllegalAccessException В случае отсутствия доступа к полю.
+     */
     private Map<String, Object> extractFieldsValues(Object object) throws IllegalAccessException {
         Map<String, Object> fieldsValues = new HashMap<>();
 
@@ -55,5 +66,19 @@ public class AssertTool {
             fieldsValues.put(field.getName(), field.get(object));
         }
         return fieldsValues;
+    }
+
+    private void addFailedAssert(String message) {
+        log.info(message);
+        failedAssertBuilder.append("\n").append(message);
+    }
+
+    public String getFailedAsserts() {
+        return failedAssertBuilder.toString();
+    }
+
+    public AssertTool setSkipMissingFields(boolean skipMissingFields) {
+        this.skipMissingFields = skipMissingFields;
+        return this;
     }
 }
